709eda693ea892dc66c7e1fe20f311c67ee0be68
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2014-04-09 00:54:36 +0200

    add dev/non dev mode and refactor a bit

--- server.go
+++ server.go
@@ -9,27 +9,45 @@ import (
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		page := app.QuotePage{Quotes: app.QuoteStore{DataFolder: "./data/"}.GetAllByDocumentedDateDesc()}
-		t, _ := template.ParseFiles("public/index.html")
-		t.Execute(w, page)
+		renderQuotePage(w)
 	} else if r.Method == "POST" {
-		store := app.QuoteStore{DataFolder: "./data/"}
-		quote := app.Quote{
-			Content:        r.FormValue("quote"),
-			Author:         r.FormValue("author"),
-			Documentor:     "Unknown",
-			DocumentedDate: time.Now()}
-		store.Save(quote)
+		storeQuote(w, r.FormValue("quote"), r.FormValue("author"))
 		http.Redirect(w, r, "/", 302)
 	}
 }
 
-func faviconHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func main() {
 	http.HandleFunc("/", viewHandler)
-	http.HandleFunc("/favicon.ico", faviconHandler)
 	http.ListenAndServe(":4000", nil)
+}
+
+func storeQuote(w http.ResponseWriter, content string, author string) {
+	quote := app.Quote{
+		Content:        content,
+		Author:         author,
+		Documentor:     "Unknown",
+		DocumentedDate: time.Now()}
+	makeQuoteStore().Save(quote)
+}
+
+var templates = template.Must(template.ParseFiles("public/index.html"))
+
+func renderQuotePage(w http.ResponseWriter) {
+	page := app.QuotePage{Quotes: makeQuoteStore().GetAllByDocumentedDateDesc()}
+
+	// false: cache views, true: don't cache views
+	devMode := true
+	if devMode {
+		t, _ := template.ParseFiles("public/index.html")
+		t.Execute(w, page)
+	} else {
+		err := templates.ExecuteTemplate(w, "index.html", page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func makeQuoteStore() app.QuoteStore {
+	return app.QuoteStore{DataFolder: "./data/"}
 }
