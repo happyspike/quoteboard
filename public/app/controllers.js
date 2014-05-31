9d1ba83f9ddb6c56cf85abaf39b3554a317323ed
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2014-05-31 19:16:05 +0200

    add styling and uservoice

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,10 +1,9 @@
 var module = angular.module("controllers", ["firebase"]);
 
 module.controller('IndexController', ['$scope', '$firebase', function($scope, $firebase) {
-  var ref = new Firebase("https://quoteboard.firebaseio.com/");
-  $scope.newQuote = {};
-  $scope.quotes = $firebase(ref);
+  var ref = new Firebase(FBURL + '/quotes');
   
+  $scope.quotes = $firebase(ref);
   $scope.addQuote = function() {
     $scope.quotes.$add($scope.newQuote);
     $scope.newQuote = {};
